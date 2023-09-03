785c0f91df30ee9add51b1ec59dbd3e2174cd8d1
shubhanshu-0 <shubhanshusaraf@SHUBHANSHUs-MacBook-Pro.local>
2023-09-03 12:37:22 +0530

    chnages in Map

--- src/components/MapIntegration/Map.js
+++ src/components/MapIntegration/Map.js
@@ -53,8 +53,8 @@ const Map = () => {
       >
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3718.542965513324!2d81.60381267595187!3d21.249964580142944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a28dd4e5d1769d3%3A0xb1134051e16f6178!2sCCC%20NIT%20Raipur!5e0!3m2!1sen!2sin!4v1693652233129!5m2!1sen!2sin"
-          width="300"
-          height="250"
+          width="230"
+          height="200"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
