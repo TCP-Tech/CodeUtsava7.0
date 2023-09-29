5f740a5695812886ab649bf6cda44a80f6a6d62a
Adarsh Kumar <54439573+Adarshkumar03@users.noreply.github.com>
2023-09-29 19:39:22 +0530

    Update EventCard.js

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -4,14 +4,13 @@ import spiderWeb from "../../assets/images/spider-web.svg";
 const EventCard = ({ img, title, date, link }) => {
   return (
     <div className="codeutsava__section3-card">
-      <div className="codeutsava__section3-decor-web one"><img src={spiderWeb} alt="spiderweb" /></div>
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <img className="codeutsava__section3-image" src={img} alt="" style={{cursor:"pointer"}}/>
-        </a>      
+      <div className="codeutsava__section3-decor-web"><img src={spiderWeb} alt="spiderweb" /></div>
+      <img className="codeutsava__section3-image" src={img} alt=""/>  
       <div className="codeutsava__section3-content">
         <div className="codeutsava__section3-cardTitle">{title}</div>
         <div className="codeutsava__section3-divider"></div>
         <div className="codeutsava__section3-date">{date}</div>
+        <a className="codeutsava__section3-link" href={link} target="_blank" rel="noreferrer">Register Now!!</a>
       </div>
     </div>
   );
