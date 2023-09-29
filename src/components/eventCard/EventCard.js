32cbc114b10db86e6ef55a5a88694e91b0064e63
Viraj Chandra <virajchandra51@gmail.com>
2023-09-30 03:02:20 +0530

    event Card

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -10,7 +10,7 @@ const EventCard = ({ img, title, date, link }) => {
         <div className="codeutsava__section3-cardTitle">{title}</div>
         <div className="codeutsava__section3-divider"></div>
         <div className="codeutsava__section3-date">{date}</div>
-        <a className="codeutsava__section3-link" href={link} target="_blank" rel="noreferrer">Register Now!!</a>
+        <a className="codeutsava__section3-link" href={link} target="_blank" rel="noreferrer">Register Now</a>
       </div>
     </div>
   );
