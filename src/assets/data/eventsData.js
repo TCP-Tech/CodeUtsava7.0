85d8948b58469fffe65952c6d175f93944451554
Viraj Chandra <virajchandra51@gmail.com>
2023-10-02 13:16:43 +0530

    Major Changes+ canvas+ different page CF

--- src/assets/data/eventsData.js
+++ src/assets/data/eventsData.js
@@ -1,11 +1,6 @@
-import event1 from "../images/event1.png";
 import event2 from "../images/event2.png";
-import event3 from "../images/event3.png";
-import event4 from "../images/event4.png";
 import event5 from "../images/event5.png";
 import event6 from "../images/event6.png";
-import event7 from "../images/event7.png";
-import event8 from "../images/event8.png";
 import event9 from "../images/event9.png";
 
 const events = [
@@ -22,27 +17,9 @@ const events = [
     link: "",
   },
   {
-    title: "IT Quiz",
-    date: "11th February",
-    img: event8,
-    link: "",
-  },
-  {
     title: "Logo Design",
     date: "12th February",
     img: event9,
-    link: "",
-  },
-  {
-    title: "Ultimate Gaming",
-    date: "11th and 12th February",
-    img: event7,
-    link: "",
-  },
-  {
-    title: "Cloud Techniques and Placement Tips",
-    date: "14th and 15th January",
-    img: event1,
     link: "",
   },
   {
@@ -50,18 +27,6 @@ const events = [
     date: "11th and 12th February",
     img: event2,
     link: "",
-  },
-  {
-    title: "CP and Algo Bootcamp",
-    date: "21st and 22nd January",
-    img: event3,
-    link: "",
-  },
-  {
-    title: "AI and Machine Learning",
-    date: "28th and 29th January",
-    img: event4,
-    link: "https://forms.gle/zetADszoQMTad7JV8",
   },
 ];
 
