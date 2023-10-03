2fb616f0c42a8affe2205b14f00da128203d71b7
Aniket Kumar <kraniket123654@gmail.com>
2023-10-03 08:06:08 +0530

    increased graph size 2

--- src/pages/home/sections/section14/Section14.js
+++ src/pages/home/sections/section14/Section14.js
@@ -40,7 +40,7 @@ const Section14 = () => {
   };
 
   const getChartHeight = () => {
-    const smallScreenHeight = 300;
+    const smallScreenHeight = 250;
     const largeScreenHeight = 400;
   
     if (window.innerWidth <= 450) {
