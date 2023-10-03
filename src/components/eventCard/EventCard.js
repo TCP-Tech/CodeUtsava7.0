bd7cded11d1592954c9ac55140c8c5555f4a6c23
Harsh Pastor <harshpastor@gmail.com>
2023-10-04 03:06:36 +0530

    Event card + extra old codeutsava logo removed(star wars themed)

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -4,11 +4,10 @@ import spiderWeb from "../../assets/images/spider-web.svg";
 const EventCard = ({ img, title, date, link }) => {
   return (
     <div className="codeutsava__section3-card">
-      <div className="codeutsava__section3-decor-web"><img src={spiderWeb} alt="spiderweb" /></div>
       <img className="codeutsava__section3-image" src={img} alt=""/>  
+        <div className="codeutsava__section3-divider"></div>
       <div className="codeutsava__section3-content">
         <div className="codeutsava__section3-cardTitle">{title}</div>
-        <div className="codeutsava__section3-divider"></div>
         <div className="codeutsava__section3-date">{date}</div>
         <a className="codeutsava__section3-link" href={link} target="_blank" rel="noreferrer">Register Now</a>
       </div>
