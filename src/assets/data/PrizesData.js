2fa12054c923039b43347344900ed44ccd959846
Viraj Chandra <virajchandra51@gmail.com>
2023-10-04 16:34:30 +0530

    prize cards

--- src/assets/data/PrizesData.js
+++ src/assets/data/PrizesData.js
@@ -1,72 +1,72 @@
 const Prizes = [
-    {
-        title : "Winner Team",
-        cashPrize : [
-            'Team will be awarded prizes worth Rs. 2.7 Lakhs.',
-            'Cash Prize of Rs. 35,000.',
-        ],
-        prizeInfo : [
-            'Digital Ocean Credits worth Rs. 50,000.',
-            'JetBrains Free Educational License for all.',
-            'Taskade Unlimited Workspace Upgrade worth Rs. 1,00,000. (Lifetime Access)',
-            '1-month FREE access to Premium Plan features of the Echo-3D platform worth Rs. 50,000.',
-            'UNO interview plan with unlimited practice from Interview Buddy worth Rs. 7000.',
-            'Discount on PrepBytes Placement Courses worth Rs. 30,000. T-Shirt Swag for all team members.',
-        ]
-    },
-    {
-        title : "Runner-up Team",
-        cashPrize : [
-            'Team will be awarded prizes worth Rs. 2.5 Lakhs.',
-            'Cash Prize of Rs. 25,000.',
-        ],
-        prizeInfo : [
-            'Digital Ocean credits worth Rs. 40,000.',
-            'JetBrains Free Educational License for all.',
-            'Taskade Unlimited Workspace Upgrade worth Rs. 1,00,000. (Lifetime Access)',
-            '1-month FREE access to Premium Plan features of the Echo-3D platform worth Rs. 50,000.',
-            'UNO interview plan with unlimited practice from Interview Buddy worth Rs. 7000.',
-            'Discount on PrepBytes Placement Courses worth Rs. 30,000. T-Shirt Swag for all team members.',
-        ]
-    },
-    {
-        title : "Second Runner-up Team",
-        cashPrize : [
-            'Team will be awarded prizes worth Rs. 2.3 Lakhs.',
-            'Cash Prize of Rs. 15,000.',
-        ],
-        prizeInfo : [
-            'Digital Ocean credits worth Rs. 33,000.',
-            'JetBrains Free Educational License for all.',
-            'Taskade Unlimited Workspace Upgrade worth Rs. 1,00,000. (Lifetime Access)',
-            '1-month FREE access to Premium Plan features of the Echo-3D platform worth Rs. 50,000.',
-            'UNO interview plan with unlimited practice from Interview Buddy worth Rs. 7000.',
-            'Discount on PrepBytes Placement Courses worth Rs. 30,000. T-Shirt Swag for all team members.',
-        ]
-    },
-    // {
-    //     title : "Winning Team of each category (Five Categories)",
-    //     cashPrize : [
-    //         'Team will be awarded prizes worth Rs. 2 Lakh.',
-    //         'Cash Prize of Rs. 10,000.',
-    //     ],
-    //     prizeInfo : [
-    //         'Digital Ocean credits worth Rs. 8,000.',
-    //         'JetBrains Free Educational License for all.',
-    //         'Taskade Unlimited Workspace Upgrade worth Rs. 1,00,000. (Lifetime Access)',
-    //         '1-month FREE access to Premium Plan features of the Echo-3D platform worth Rs. 50,000.',
-    //         'UNO interview plan with unlimited practice from Interview Buddy worth Rs. 7000.',
-    //         'Discount on PrepBytes Placement Courses worth Rs. 30,000.',
-    //     ]
-    // },
-    // {
-    //     title : "Extras",
-    //     cashPrize : [
-    //     ],
-    //     prizeInfo : [
-    //         'T-Shirts, Stickers and other goodies for all participants.',
-    //     ],
-    // },
+  {
+    title: "🥇 Winner Team 🥇",
+    cashPrize: [
+      "The winning team will be awarded Rs. 2.7 Lakhs, with Rs. 35,000 in cash.",
+    ],
+    prizeInfo: [
+      "Digital Ocean Credits: Rs. 50,000",
+      "JetBrains Free Educational License",
+      "Taskade Unlimited Workspace Upgrade: Rs. 1,00,000",
+      "1-month FREE Echo-3D Premium Plan: Rs. 50,000",
+      "UNO interview plan by Interview Buddy: Rs. 7,000",
+      "Discount on PrepBytes Placement Courses: Rs. 30,000",
+      "T-Shirt Swag for all team members",
+    ],
+  },
+  {
+    title: "🥈 Runner - Up Team 🥈",
+    cashPrize: [
+      "The runner-up team will be awarded Rs. 2.5 Lakhs, with Rs. 25,000 in cash.",
+    ],
+    prizeInfo: [
+      "Digital Ocean Credits: Rs. 40,000",
+      "JetBrains Free Educational License",
+      "Taskade Unlimited Workspace Upgrade: Rs. 1,00,000",
+      "1-month FREE Echo-3D Premium Plan: Rs. 50,000",
+      "UNO interview plan by Interview Buddy: Rs. 7,000",
+      "Discount on PrepBytes Placement Courses: Rs. 30,000",
+      "T-Shirt Swag for all team members",
+    ],
+  },
+  {
+    title: "🥉 Second Runner - Up Team 🥉",
+    cashPrize: [
+      "The second runner-up team will be awarded Rs. 2.3 Lakhs, with Rs. 15,000 in cash.",
+    ],
+    prizeInfo: [
+      "Digital Ocean Credits: Rs. 33,000",
+      "JetBrains Free Educational License",
+      "Taskade Unlimited Workspace Upgrade: Rs. 1,00,000",
+      "1-month FREE Echo-3D Premium Plan: Rs. 50,000",
+      "UNO interview plan by Interview Buddy: Rs. 7,000",
+      "Discount on PrepBytes Placement Courses: Rs. 30,000",
+      "T-Shirt Swag for all team members",
+    ],
+  },
+  // {
+  //     title : "Winning Team of each category (Five Categories)",
+  //     cashPrize : [
+  //         'Team will be awarded prizes worth Rs. 2 Lakh.',
+  //         'Cash Prize of Rs. 10,000.',
+  //     ],
+  //     prizeInfo : [
+  //         'Digital Ocean credits worth Rs. 8,000.',
+  //         'JetBrains Free Educational License for all.',
+  //         'Taskade Unlimited Workspace Upgrade worth Rs. 1,00,000. (Lifetime Access)',
+  //         '1-month FREE access to Premium Plan features of the Echo-3D platform worth Rs. 50,000.',
+  //         'UNO interview plan with unlimited practice from Interview Buddy worth Rs. 7000.',
+  //         'Discount on PrepBytes Placement Courses worth Rs. 30,000.',
+  //     ]
+  // },
+  // {
+  //     title : "Extras",
+  //     cashPrize : [
+  //     ],
+  //     prizeInfo : [
+  //         'T-Shirts, Stickers and other goodies for all participants.',
+  //     ],
+  // },
 ];
 
 export default Prizes;
