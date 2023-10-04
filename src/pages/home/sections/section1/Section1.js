2b81d78c6dad60f2a718d787c83e6d71cbe8cfc9
Viraj Chandra <virajchandra51@gmail.com>
2023-10-04 23:08:32 +0530

    stats

--- src/pages/home/sections/section1/Section1.js
+++ src/pages/home/sections/section1/Section1.js
@@ -31,7 +31,7 @@ const Section1 = () => {
           </div>
           <div className="codeutsava__section1-para">
             Central India's{" "}
-            <span style={{ fontWeight: "600" }}>Largst Coding Event.</span> Coming Soon!
+            <span style={{ fontWeight: "600" }}>Largest Coding Event.</span> Coming Soon!
           </div>
         </div>
 
