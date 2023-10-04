117dffa719db9e7a2b0ff0474ebc665bcbf29081
Viraj Chandra <virajchandra51@gmail.com>
2023-10-04 23:01:44 +0530

    message

--- src/pages/home/sections/section1/Section1.js
+++ src/pages/home/sections/section1/Section1.js
@@ -31,8 +31,7 @@ const Section1 = () => {
           </div>
           <div className="codeutsava__section1-para">
             Central India's{" "}
-            <span style={{ fontWeight: "600" }}>Largst Coding Event.</span> Join
-            Us on 1st - 2nd November.
+            <span style={{ fontWeight: "600" }}>Largst Coding Event.</span> Coming Soon!
           </div>
         </div>
 
